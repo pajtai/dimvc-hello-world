6a58c4174b5e93f97dea3941d902fd38a67f563c
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-10-14 22:43:25 -0700

    user updates

--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -1,11 +1,13 @@
 /*jshint node:true */
 'use strict';
 
+var sillyname = require('sillyname');
+
 module.exports = function(mongoose) {
 
     var Schema = mongoose.Schema,
         schema = new Schema({
-            name: {type: String, trim: true}
+            name: {type: String, trim: true, default : sillyname }
         });
 
     return mongoose.model('users', schema);
